64624f965d893c512989e38d5f4e4cf3347b913a
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-21 14:53:16 -0600

    Update Task1.sql

--- Task1.sql
+++ Task1.sql
@@ -1,3 +1,22 @@
-/*Select the top 10 counties that have EV resgitered in WA.*/
-SELECT MIN(year_founded) AS MIN, MAX(year_founded) AS Max
-FROM businesses;
+/*Select the top 10 Counties/Cities with EVs resgitered in WA.*/
+-- SQL Query using BigQuery 
+
+--County  
+SELECT
+  county,
+  COUNT(county) AS amt_of_cars
+FROM
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY county
+ORDER BY COUNT(county) DESC
+LIMIT 10
+
+--City
+SELECT 
+  city,
+  COUNT(City) AS Count
+FROM 
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY City
+ORDER BY COUNT(City) DESC 
+LIMIT 10
