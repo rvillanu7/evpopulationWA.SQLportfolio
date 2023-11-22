afb6947e6baacee6b62def0d4e4d0ee5ae1f43a7
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-22 07:21:27 -0600

    Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -1,1 +1,24 @@
-/*Select the top/least 10 make and model that have most regsitered EV in WA .*/
+/*Select the top/least 10, make and models that are regsitered EVs in WA .*/
+-- SQL Query using BigQuery
+
+--Top 10 Make & Models
+SELECT
+  make,
+  model,
+  COUNT(model) AS models_registered_in_wa
+FROM
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY model, Make
+ORDER BY COUNT(model) DESC
+LIMIT 10
+  
+--Least 10 Make & Models
+SELECT
+  make,
+  model,
+  COUNT(model) AS models_registered_in_wa
+FROM
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY model, Make
+ORDER BY COUNT(model)
+LIMIT 10
