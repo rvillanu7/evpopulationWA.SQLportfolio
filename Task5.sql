a420130427277d544c956654d139b2350aaa0e79
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-22 06:58:12 -0600

    Task5.sql

--- Task5.sql
+++ Task5.sql
@@ -1,1 +1,25 @@
-/*Select the average range for the least resgietered car in WA.*/
+/*Find the average range for the car least registered in WA.*/
+-- SQL Query using BigQuery
+
+/*Find the models least registered*/
+SELECT 
+make,
+model,  
+count(model) AS cnt_model
+FROM
+`celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY model, make
+HAVING count(model)=1
+ORDER BY make
+
+/*Average Range of Model*/
+SELECT
+make,
+model,
+AVG(Electric_Range) AS avg_range,
+COUNT(Model) AS cnt_model
+FROM
+`celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY make, model
+HAVING count(model) = 1
+ORDER BY make
