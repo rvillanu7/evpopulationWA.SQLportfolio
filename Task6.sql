9a260b3778b536a1468c66894b73d26fac6a4376
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-22 07:12:39 -0600

    Task6.sql

--- Task6.sql
+++ Task6.sql
@@ -1,1 +1,25 @@
-/*Select the max range/ avg. leaset range.*/
+/*Find the max and min range.*/
+-- SQL Query using BigQuery
+
+--Max Range
+SELECT
+  make, 
+  model, 
+  MAX(Electric_Range) AS max_range
+FROM
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY make, model, Electric_Range
+ORDER BY ROUND(avg(Electric_Range)) DESC
+LIMIT 1
+
+--Min Range
+SELECT
+  make, 
+  model, 
+  MIN(Electric_Range) AS min_range
+FROM
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
+GROUP BY make, model, Electric_Range
+HAVING Electric_Range <> 0
+ORDER BY ROUND(avg(Electric_Range)) 
+LIMIT 1
