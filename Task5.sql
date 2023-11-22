0e42fa6caef9f82f52159b58b966e7ad8584be98
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-22 07:24:06 -0600

    Task5.sql

--- Task5.sql
+++ Task5.sql
@@ -3,23 +3,23 @@
 
 /*Find the models least registered*/
 SELECT 
-make,
-model,  
-count(model) AS cnt_model
+  make,
+  model,  
+  COUNT(model) AS cnt_model
 FROM
-`celestial-feat-399513.evpop.electric_vehicle_pop`
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
 GROUP BY model, make
 HAVING count(model)=1
 ORDER BY make
 
 /*Average Range of Model*/
 SELECT
-make,
-model,
-AVG(Electric_Range) AS avg_range,
-COUNT(Model) AS cnt_model
+  make,
+  model,
+  AVG(Electric_Range) AS avg_range,
+  COUNT(Model) AS cnt_model
 FROM
-`celestial-feat-399513.evpop.electric_vehicle_pop`
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
 GROUP BY make, model
 HAVING count(model) = 1
 ORDER BY make
