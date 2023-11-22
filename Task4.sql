eb39f00ca568ca7960633d05e36c2700e2f4ea1d
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-22 07:23:30 -0600

    Task4.sql

--- Task4.sql
+++ Task4.sql
@@ -3,22 +3,22 @@
 
 /*Find the Model most registered*/
 SELECT 
-make,
-model,  
+  make,
+  model,  
 count(model) AS cnt_model
 FROM
-`celestial-feat-399513.evpop.electric_vehicle_pop`
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
 group by model, make
 order by count(model) DESC
 LIMIT 1
 
 /*Average Range of Model*/
 SELECT
-make,
-model, 
-AVG(Electric_Range) AS average_range
+  make,
+  model, 
+  AVG(Electric_Range) AS average_range
 FROM
-`celestial-feat-399513.evpop.electric_vehicle_pop`
+  `celestial-feat-399513.evpop.electric_vehicle_pop`
 WHERE model = "MODEL Y"
 GROUP BY make, model, electric_range
 LIMIT 1
